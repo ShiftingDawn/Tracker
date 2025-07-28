a9ba507ef5bdf65e96b1f36c7f3925c8a698ae01
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-07-28 22:48:05 +0200

    Add auth database migration

---
+++ drizzle/0000_auth.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "session" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"username" varchar(24) NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "user_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
