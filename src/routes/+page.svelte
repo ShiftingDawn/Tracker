a4372aaaae8c25b1574d59c2794494c5d2d3231a
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-07-28 22:47:09 +0200

    Added a nicer homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Section from "$lib/componments/section.svelte";
+
+  const { data } = $props();
+</script>
+
+<Section title="Welcome to Tracker!" w="md">
+  <p>Tracker is a customizable progress tracker, scoreboard and more!</p>
+  {#if data.isLoggedIn}
+    <p class="mt-4">
+      Welcome back, <strong>{data.user.username}</strong>
+    </p>
+  {:else}
+    <p class="mt-4">Sign in to get started...</p>
+  {/if}
+</Section>
