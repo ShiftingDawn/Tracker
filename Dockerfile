a1e552ae03f84aa07938598f32a1c64559e26a4e
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2026-07-19 12:19:09 +0200

    Add docker build

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM oven/bun:1 AS builder
+
+WORKDIR /app
+
+COPY package.json bun.lock ./
+RUN bun install --frozen-lockfile
+
+COPY . .
+
+ENV IS_BUILD_ENV=true
+RUN bun run build
+RUN bun install --frozen-lockfile --production
+
+FROM oven/bun:1
+
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+COPY --from=builder /app/build ./build
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./
+COPY --from=builder /app/drizzle ./drizzle
+COPY --from=builder /app/drizzle.config.ts ./
+
+ENV DATABASE_URL=postgres://root:mysecretpassword@localhost:5432/local
+ENV REDIS_URL=redis://192.168.1.2:6379/1
+
+ENV PORT=3000
+ENV ORIGIN=http://localhost:$PORT/
+
+EXPOSE $PORT
+
+CMD ["bun", "run", "prod"]
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,13 +1,15 @@
-import { env } from "$env/dynamic/private";
-import { drizzle } from "drizzle-orm/postgres-js";
+import {env} from "$env/dynamic/private";
+import {drizzle} from "drizzle-orm/postgres-js";
 import Redis from "ioredis";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-if (!env.REDIS_URL) throw new Error("REDIS_URL is not set");
+if (env.IS_BUILD_ENV !== "true") {
+  if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+  if (!env.REDIS_URL) throw new Error("REDIS_URL is not set");
+}
 
 const client = postgres(env.DATABASE_URL);
 
-export const db = drizzle(client, { schema, });
+export const db = drizzle(client, {schema,});
 export const redis = new Redis(env.REDIS_URL);
